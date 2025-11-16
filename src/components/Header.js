551da8b223a223e07c0439b10f9459a707ab2cbf
vikaskalyan-rgb <vikaskalyan1102@gmail.com>
2025-11-16 13:28:19 +0530

    esgerwgy

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function Header () {
   
-
   const scrollToLocation = (e) => {
     e.preventDefault();
     const locationSection = document.getElementById('whenwhere');
@@ -25,19 +24,39 @@ function Header () {
           <div className='col-md-12 text-center caption'>
             <div className='hero-content'>
               
-              {/* Main couple names */}
-              <h1 className='animate-box hero-names' data-animate-effect='fadeInUp'>
-                Varsha & Vikas
-              </h1>
+              {/* Names and Parents Layout */}
+              <div className='names-section'>
+                {/* Bride Section */}
+                <div className='bride-section'>
+                  <h1 className='name-text animate-box' data-animate-effect='fadeInUp'>
+                    Varsha
+                  </h1>
+                  <p className='parent-label'>DAUGHTER OF</p>
+                  <p className='parent-names'>Mr. K. Ravi & Mrs. Krithika Ravi</p>
+                </div>
+
+                {/* Ampersand */}
+                <div className='ampersand-wrapper'>
+                  <span className='ampersand-large'>&</span>
+                </div>
+
+                {/* Groom Section */}
+                <div className='groom-section'>
+                  <h1 className='name-text animate-box' data-animate-effect='fadeInUp'>
+                    Vikas
+                  </h1>
+                  <p className='parent-label'>SON OF</p>
+                  <p className='parent-names'>Mr. N. Kalyan & Mrs. Gayathri Kalyan</p>
+                </div>
+              </div>
 
               {/* Elegant divider */}
               <div className='hero-divider'>
                 <span className='divider-line-left'></span>
-                 <span className='divider-icon'>♥</span>
+                <span className='divider-icon'>♥</span>
                 <span className='divider-line-right'></span>
               </div>
             
-
               {/* Subtitle */}
               <p className='hero-subtitle animate-box' data-animate-effect='fadeInUp'>
                 Together Forever, From This Day Forward
@@ -55,7 +74,6 @@ function Header () {
 
                 <div className='event-card animate-box' data-animate-effect='fadeInUp'>
                   <div className='event-card-inner'>
-            
                     <h3 className='event-title'>Marriage Ceremony</h3>
                     <div className='event-date'>March 05, 2026</div>
                     <div className='event-time'>11:00 AM - 12:00 PM</div>
@@ -72,14 +90,10 @@ function Header () {
                   <i className='ti-location-pin'></i>
                   View Location
                 </button>
-              
               </div>
             </div>
           </div>
         </div>
-
-        {/* Scroll indicator */}
-        
       </div>
     </header>
   )
